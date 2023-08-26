2196c4d3e2f70d07c840434cda633c195411191f
Henryk Trappmann <stromzaehler@orangepipc>
2023-08-26 10:12:48 +0200

    stromzaehler-send-values.sh sends now to influx2 cloud

--- stromzaehler-send-values.sh
+++ stromzaehler-send-values.sh
@@ -15,25 +15,43 @@ METER_280=${a_b#* }
 #Counter,idx=113,name=stromzaehler-aus-2.8.0
 #Counter,idx=114,name=stromzaehler-ein-1.8.0
 #Counter,idx=115,name=stromzaehler
+INFLUX_URL=https://eu-central-1-1.aws.cloud2.influxdata.com
+INFLUX_TOKEN=rnBcm2cHFZGEvax6KlxTXSeoyF3gGmm5KpyeRx1MzlGB2gW7tz_PTWQo0exthhEOkCK2lU6QxxFR92elg7GkjQ==
+INFLUX_ORG=99702231a4477a51
+INFLUX_BUCKET=oranienbaum-energy
+
+function influx2_write_line {
+  local line="$1"
+  #Counter,idx=114,name=stromzaehler-ein-1.8.0  value=$METER_180
+  curl -sS -X POST\
+    -H "Authorization: Token $INFLUX_TOKEN"\
+    -H "Content-Type: text/plain; charset=utf-8"\
+    -H "Accept: application/json"\
+    -d "$line"\
+    "$INFLUX_URL/api/v2/write?bucket=$INFLUX_BUCKET"
+}
 
 if [[ $METER_180 =~ ^[0-9]+$ ]]
 then
   echo "Meter 1.8.0 (from plant):    " $METER_180 "kWh" 1>&2
-  curl -sS -X POST -d "Counter,idx=114,name=stromzaehler-ein-1.8.0  value=$METER_180" "192.168.178.101:8086/write?db=domoticz"
+  influx2_write_line "Counter,idx=114,name=stromzaehler-ein-1.8.0  value=$METER_180"
+  #curl -sS -X POST -d "Counter,idx=114,name=stromzaehler-ein-1.8.0  value=$METER_180" "192.168.178.101:8086/write?db=domoticz"
 #  curl -sS "192.168.178.101:8080/json.htm?type=command&param=udevice&idx=114&svalue=$METER_180"
 fi
 
 if [[ $METER_280 =~ ^[0-9]+$ ]]
 then
   echo "Meter 2.8.0 (to plant):      " $METER_280 "kWh" 1>&2
-  curl -sS -X POST -d "Counter,idx=113,name=stromzaehler-ein-2.8.0  value=$METER_280" "192.168.178.101:8086/write?db=domoticz"
+  influx2_write_line "Counter,idx=113,name=stromzaehler-ein-2.8.0  value=$METER_280"
+  #curl -sS -X POST -d "Counter,idx=113,name=stromzaehler-ein-2.8.0  value=$METER_280" "192.168.178.101:8086/write?db=domoticz"
 #  curl -sS "192.168.178.101:8080/json.htm?type=command&param=udevice&idx=113&svalue=$METER_280"
 fi
 
 if [[ $METER_280 =~ ^[0-9]+$ ]] && [[ $METER_180 =~ ^[0-9]+$ ]]
 then
   echo "in - out = $(( METER_280 - METER_180 ))" 1>&2
-  curl -sS -X POST -d "Counter,idx=115,name=stromzaehler  value=$(( METER_280 - METER_180 ))" "192.168.178.101:8086/write?db=domoticz"
+  influx2_write_line "Counter,idx=115,name=stromzaehler  value=$(( METER_280 - METER_180 ))"
+  #curl -sS -X POST -d "Counter,idx=115,name=stromzaehler  value=$(( METER_280 - METER_180 ))" "192.168.178.101:8086/write?db=domoticz"
 #  curl -sS "192.168.178.101:8080/json.htm?type=command&param=udevice&idx=115&svalue=$(( METER_280 - METER_180 ))"
 fi
 
